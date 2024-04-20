ddac45a8091cc2692e9102c63bf2f2a6b90ab7ed
Blythe Lockhart <blythe.lockhart@att.net>
2024-04-19 21:24:10 -0500

    corrected header for betty and improved declarations in 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,12 +8,14 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current = (*list)->next;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	listint_t *current = (*list)->next;
 	while (current != NULL)
 	{
 		listint_t *temp = current;
+
 		while (temp->prev != NULL && temp->n < temp->prev->n)
 		{
 			temp->prev->next = temp->next;
